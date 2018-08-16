c938d0e43698f0c019023c2c732d525e41ee2ac4
meckelangelo <meckelangelo@gmail.com>
2018-08-16 16:09:05 -0400

    New Icons

--- smartapps/meckelangelo/fan-led-indicator-fixer.src/fan-led-indicator-fixer.groovy
+++ smartapps/meckelangelo/fan-led-indicator-fixer.src/fan-led-indicator-fixer.groovy
@@ -4,8 +4,9 @@ definition(
     author: "David Meck",
     description: "Changes the indicator light setting to always be off.",
     category: "My Apps",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png"
+    iconUrl: "https://raw.githubusercontent.com/meckelangelo/STFanLEDFixer/master/LED60x60.png",
+    iconX2Url: "https://raw.githubusercontent.com/meckelangelo/STFanLEDFixer/master/LED120x120.png",
+    iconX3Url: "https://raw.githubusercontent.com/meckelangelo/STFanLEDFixer/master/LED.png")
 )
 
 preferences {
